d2dd4f13e36be7884bdbf57478fff6821cbbaa0c
aryan0078 <aryan8931080555@gmail.com>
2021-07-17 13:09:50 +0530

    patch for error

--- index.js
+++ index.js
@@ -43,63 +43,64 @@ const { toFancyNum } = require("./src/utils/constants.js");
 // Login. (And start in development mode if --dev is passed)
 let d = new MiyakoClient(process.argv.includes("--dev"));
 d.login();
+try {
+  require("discord-buttons")(d);
+  d.on("clickButton", async (button) => {
+    let l = d.dbClient;
+    l = l.db();
+    let args = button.id;
+    if (args.startsWith("accept")) {
+      let u = args.split(":");
+      let balance = await getdabbal(button.clicker.user.id, l);
+      let result = balance.points;
+      if (result < parseInt(u[2])) {
+        return button.message.send(
+          `You don't have enough <:dabs:851218687255773194> dabs to pay fill your wallet and then pay`
+        );
+      } else {
+        await paydab(button.clicker.user.id, u[3], parseInt(u[2]), "DM", l);
+        button.message.send(
+          `Sent **${toFancyNum(
+            parseInt(u[2])
+          )}** <:dabs:851218687255773194> dabs to **${u[3]}** `
+        );
+        button.message.delete();
+        try {
+          let user = await d.users.fetch(u[3]);
+          user.send(
+            `**${user.username} |** Payment Accepted from ${button.clicker.user.username
+            } of **${toFancyNum(
+              parseInt(u[2])
+            )}** <:dabs:851218687255773194> dabs you recived the amount is wallet!`
+          );
+        } catch (e) {
+          console.log("");
+        }
+        await button.defer();
+      }
+    }
+    if (args.startsWith("reject")) {
+      let u = args.split(":");
 
-require("discord-buttons")(d); 
-d.on("clickButton", async (button) => {
-  let l = d.dbClient;
-  l = l.db();
-  let args = button.id;
-  if (args.startsWith("accept")) {
-    let u = args.split(":");
-    let balance = await getdabbal(button.clicker.user.id, l);
-    let result = balance.points;
-    if (result < parseInt(u[2])) {
-      return button.message.send(
-        `You don't have enough <:dabs:851218687255773194> dabs to pay fill your wallet and then pay`
-      );
-    } else {
-      await paydab(button.clicker.user.id, u[3], parseInt(u[2]), "DM", l);
       button.message.send(
-        `Sent **${toFancyNum(
+        `Payment rejected of **${toFancyNum(
           parseInt(u[2])
-        )}** <:dabs:851218687255773194> dabs to **${u[3]}** `
+        )}** <:dabs:851218687255773194> dabs to **<@${u[3]}>** `
       );
       button.message.delete();
       try {
         let user = await d.users.fetch(u[3]);
         user.send(
-          `**${user.username} |** Payment Accepted from ${
-            button.clicker.user.username
-          } of **${toFancyNum(
-            parseInt(u[2])
-          )}** <:dabs:851218687255773194> dabs you recived the amount is wallet!`
+          `**${user.username} |** Payment Rejected from ${button.clicker.user.username
+          } of **${toFancyNum(parseInt(u[2]))}** <:dabs:851218687255773194> dabs`
         );
       } catch (e) {
         console.log("");
       }
       await button.defer();
     }
-  }
-   if (args.startsWith("reject")) {
-     let u = args.split(":");
-
-     button.message.send(
-       `Payment rejected of **${toFancyNum(
-         parseInt(u[2])
-       )}** <:dabs:851218687255773194> dabs to **<@${u[3]}>** `
-     );
-     button.message.delete();
-     try {
-       let user = await d.users.fetch(u[3]);
-       user.send(
-         `**${user.username} |** Payment Rejected from ${
-           button.clicker.user.username
-         } of **${toFancyNum(parseInt(u[2]))}** <:dabs:851218687255773194> dabs`
-       );
-     } catch (e) {
-       console.log("");
-     }
-     await button.defer();
-   }
    
-});
+  });
+} catch (err) {
+  console.log('Error')
+}
