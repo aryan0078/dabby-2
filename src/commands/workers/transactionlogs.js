ac0e296383b05cc649ac774ae305ec6f4e71aa70
aryan0078 <aryan8931080555@gmail.com>
2021-07-18 12:43:23 +0530

    tl patch

--- src/commands/workers/transactionlogs.js
+++ src/commands/workers/transactionlogs.js
@@ -14,29 +14,48 @@ class TransactionLogs extends Command {
     }
 
     async run(msg, [user]) {
-        if (await this.workercheck(msg) || await this.globalpartnercheck(msg) || await this.guidercheck(msg)) {
-
-
+        if (msg.channel.id == "866214211579019294") {
             user = await this.verifyUser(msg, user);
             let author = await this.verifyMember(msg, msg.author)
             let db = this.client.dbClient;
             db = await db.db();
             let logs = db.collection('transations')
-            let l = await logs.find({ by: `${user.id}` }).sort({ at: -1 }).limit(12).toArray();
+            let l = await logs.find({ by: `${user.id}` }).sort({ at: -1 }).limit(15).toArray();
             let str = `Transactions logs of ${user.username}\n`
-            l.forEach(async (lo,index) => {
-                if (index > 10) {
-                    return msg.send('Check DM for logs').then(async (m) => { await author.send(str); m.react('👍') })
-                }
+            l.forEach(async (lo, index) => {
+
                 let u = await this.client.users.fetch(lo.to)
-                str += `**${user.username}** has transferred **${toFancyNum(lo.amount)}** <:dabs:851218687255773194> dabs to **${u.username}** at ${new Date(lo.at)}\n`
+                str += `**${user.username}** has transferred **${toFancyNum(lo.amount)}** <:dabs:851218687255773194> dabs to **${u.username}** at ${new Date(lo.at).toDateString()}\n`
+                if (index == l.length - 1) {
+                    return msg.send('Logs').then(async (m) => { await msg.channel.send(str); m.react('👍') })
+                }
             });
 
-
-            // return author.send(str)
-
         } else {
-            return replyError(msg, 'This command is only for workers,guiders,global partners', 5000)
+            if (await this.workercheck(msg) || await this.globalpartnercheck(msg) || await this.guidercheck(msg)) {
+
+
+                user = await this.verifyUser(msg, user);
+                let author = await this.verifyMember(msg, msg.author)
+                let db = this.client.dbClient;
+                db = await db.db();
+                let logs = db.collection('transations')
+                let l = await logs.find({ by: `${user.id}` }).sort({ at: -1 }).limit(12).toArray();
+                let str = `Transactions logs of ${user.username}\n`
+                l.forEach(async (lo, index) => {
+                    if (index > 10) {
+                        return msg.send('Check DM for logs').then(async (m) => { await author.send(str); m.react('👍') })
+                    }
+                    let u = await this.client.users.fetch(lo.to)
+                    str += `**${user.username}** has transferred **${toFancyNum(lo.amount)}** <:dabs:851218687255773194> dabs to **${u.username}** at ${new Date(lo.at)}\n`
+                });
+
+
+                // return author.send(str)
+
+            } else {
+                return replyError(msg, 'This command is only for workers,guiders,global partners', 5000)
+            }
         }
     }
 }
